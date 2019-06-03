05caf2bf583fcc390eb8a3af25938b9ecaedcbe1
gumby <dpark543107@gmail.com>
2019-06-03 16:06:49 -0500

    added test component

---
+++ src/components/test/test.component.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Test = () => <h1>Testing from My Component</h1>;
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
+import Test from "./components/test/test.component";
+
 const MyComponent = () => <h1>Hello from My Component</h1>;
-export { MyComponent };
+
+export { MyComponent, Test };
