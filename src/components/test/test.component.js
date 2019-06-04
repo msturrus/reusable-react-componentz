ee00c1503f3dd05d5cfa38952054c8312b5d896e
gumby <dpark543107@gmail.com>
2019-06-03 21:19:46 -0500

    will this work

--- src/components/test/test.component.js
+++ src/components/test/test.component.js
@@ -1,4 +1,4 @@
 import React from "react";
 
-const Test = () => <h1>Testing from My Component</h1>;
+const Test = () => <h1>Testing from My Component, testing this better work</h1>;
 export default Test;
